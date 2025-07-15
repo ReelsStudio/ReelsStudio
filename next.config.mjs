c21f19f7948c7553ea19f7714e7450a411065378
ReelsStudio <reelsstudio34@gmail.com>
2025-07-15 15:08:57 +0200

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['am.net.ua', 'img.youtube.com', 'cdn.sanity.io'],
-  },
-  experimental: {
-    turbo: false,
-  },
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
+    images: {
+      domains: ['am.net.ua', 'img.youtube.com', 'cdn.sanity.io'],
+    },
 
-};
 
-export default nextConfig;
+  };
+
+  export default nextConfig;
